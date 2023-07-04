c2075b2ecb8374b97a81807de9ed5da38edd1743
Valisher Botirov <valisherbotirov1218@gmail.com>
2023-07-04 20:02:04 +0500

    add product page

--- src/store/auth.js
+++ src/store/auth.js
@@ -1,22 +1,18 @@
 import { defineStore } from "pinia";
 import axios from "axios";
 
-
 export const useAuthStore = defineStore("auth", {
-    state: () => ({
-
-        user:
-            {
-                login:"admin",
-                parol:"Parol123",
-                userRole : '',
-                isRegister:0
-            }
-        ,
-    }),
-    actions: {
-        checkUser(){
-            return this.user
-        }
+  state: () => ({
+    user: {
+      login: "admin",
+      parol: "Parol123",
+      userRole: "",
+      isRegister: 0,
     },
+  }),
+  actions: {
+    checkUser() {
+      return this.user;
+    },
+  },
 });
